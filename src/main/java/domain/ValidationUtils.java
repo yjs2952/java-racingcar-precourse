baa202d5f63b52ad131fc1aacb5c487711b2d99a
yjs2952 <yjs2952@naver.com>
2021-05-03 23:17:30 +0900

    refactor: 이름 최대길이, 공백 상수 처리

--- src/main/java/domain/ValidationUtils.java
+++ src/main/java/domain/ValidationUtils.java
@@ -2,15 +2,18 @@ package domain;
 
 public class ValidationUtils {
 
+    public static final int MAX_LENGTH = 5;
+    public static final String BLANK = " ";
+
     public static boolean validDriverName(String driverName) {
         return isValidString(driverName) && isValidLength(driverName) ;
     }
 
     private static boolean isValidString(String driverName) {
-        return driverName != null && !driverName.contains(" ");
+        return driverName != null && !driverName.contains(BLANK);
     }
 
     private static boolean isValidLength(String driverName) {
-        return !driverName.isEmpty() && driverName.length() <= 5;
+        return !driverName.isEmpty() && driverName.length() <= MAX_LENGTH;
     }
 }
